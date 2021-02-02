a86c5767babc50be3cca8c193288164ab40c135d
bhupathi2628 <77353897+bhupathi2628@users.noreply.github.com>
2021-02-02 21:40:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@
 #   }
 # }
 
-# Configure the AWS Provider
-# provider "aws" {
-#   region = "us-east-1"
-# }
+Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
 
 resource "aws_vpc" "main_vpc" {
   cidr_block       = "10.0.0.0/16"
